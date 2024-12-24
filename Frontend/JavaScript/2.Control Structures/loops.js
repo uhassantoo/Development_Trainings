64c67502fac197ec7146db46cf96f32a19428755
uhassantoo <uhassantoor@gmail.com>
2024-12-24 13:00:03 +0500

    3. Functions

--- Frontend/JavaScript/2.Control Structures/loops.js
+++ Frontend/JavaScript/2.Control Structures/loops.js
@@ -1,11 +1,40 @@
-// Loops
+// // Loops
 
 
 
-for(let i = 5 ; i<=10 ; i++){
-    console.log('Umer Hassan')
+// for(let i = 5 ; i<=10 ; i++){
+//     console.log('Umer Hassan')
+// }
+
+
+// for(let a = 0; a<=20; a=a+1){
+//    console.log(a)
+   
+// }
+
+
+y = 5  // Variable Initialize
+// while (y<=40) {
+//    console.log('Hellow ')
+//    y++
+// }
+
+// do{
+//  console.log(y)
+//   break;
+// }while(y<=20)
+    
+let arr = [45,55,60,40,30]
+console.log(arr)
+for(a of arr){
+   console.log(a)
 }
-//  i = 5;
-// for(i = 5)  // variable initialize
-    i<=10 // Condition
-    i++ // incremnet operator
+
+let obj = {
+   name : 'Umer',
+   lastname : 'Hassan',
+   age : 29,
+}
+for (key in obj){
+   console.log(key)
+}
---
+++ Frontend/JavaScript/3. Functions/fun.js
@@ -0,0 +1,40 @@
+function greet(){
+    console.log('Happy Christmas')
+}
+greet() // function calling
+
+function add(a,b){
+    num = a+b
+    console.log("happy Quaid Day", num)
+ 
+}
+add(6,10,20)
+add(4,10)
+add(6,6)
+add(7,8)
+
+
+//Return Statement 
+
+function square(num){
+    return num * num 
+}
+
+let add1 = square(20)
+console.log('Square is ', add1)
+
+// JavaScript Library Functions
+// JavaScript provides some built-in functions that can be directly used in our program. We don't need to create these functions; we just need to call them.
+
+// Some common JavaScript library functions are:
+
+// Library Function	Description
+// console.log()	Prints the string inside the quotation marks.
+// Math.sqrt()	Returns the square root of a number.
+// Math.pow()	Returns the power of a number.
+// toUpperCase()	Returns the string converted to uppercase.
+// toLowerCase()	Returns the string converted to lowercase.
+
+
+// JavaScript Arrow Function
+// JavaScript CallBack Function
