b6fd3d1ff47c164e32648bbd6180c0552d2e5d8e
uhassantoo <uhassantoor@gmail.com>
2024-12-31 12:56:40 +0500

    2. Python Fundamentals

--- Python/2. Python Fundamentals/operators.py
+++ Python/2. Python Fundamentals/operators.py
@@ -4,3 +4,16 @@
 # Logical Operators  AND, OR ,NOT
 # Bitwise Operators
 # Special Operators
+
+#Logical Operator
+#And
+num1 = 44
+num2 = 44
+print((num1>=num2) and (num2>=num1))
+
+#Or
+
+print(num1>num2 or num2>num1)
+
+#Not
+print(not(num1>=num2) and (num2>=num1))
