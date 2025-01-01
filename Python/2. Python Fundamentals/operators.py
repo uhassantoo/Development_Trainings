59655e484ca4e6f77fc578808c75076f92000fe9
uhassantoo <uhassantoor@gmail.com>
2025-01-01 11:49:52 +0500

    2. Python Fundamentals

--- Python/2. Python Fundamentals/operators.py
+++ Python/2. Python Fundamentals/operators.py
@@ -17,3 +17,19 @@ print(num1>num2 or num2>num1)
 
 #Not
 print(not(num1>=num2) and (num2>=num1))
+
+# // Special Operators
+print('Special Operator')
+myname = 'UmerHassan'
+id = 23
+print('Umer Hassan' is myname)
+print(25 is id)
+print(24 is not id)
+
+#Membership Operator
+print('Membership Operator')
+fruits = [
+    'orange', 'apple','mango','o'
+]
+print('o' in fruits)
+print('orange' not in fruits)
