bc807078b964133c33626d7c41c5200844c04e72
uhassantoo <uhassantoor@gmail.com>
2025-01-01 12:42:28 +0500

    3. Python Flow Control

---
+++ Python/3. Python Flow Control/if.py
@@ -0,0 +1,23 @@
+# if
+std_1 = 20
+std_2 = 24
+
+if std_1 > std_2:
+    print('Greater')
+else:
+    print('Smaller')
+    
+# Grade System
+
+marks = 70
+
+if marks>=80 and marks<=100:
+    print('A+ Grade')
+elif marks>=65 and marks<=79:
+    print('A Grade')
+elif marks>=55 and marks<=64:
+    print('B Grade')
+elif marks>=50 and marks<=54:
+    print('C Grade')
+else:
+    print('Failed')
---
+++ Python/3. Python Flow Control/loops.py
@@ -0,0 +1,31 @@
+# For Loops
+lang = ['HTML','CSS','JS','Python']
+for languages in lang:
+    print(languages)
+
+fruits = {
+    'Apple' : 200,
+    'Orange' : 300,
+}
+for price in fruits:
+    print(price)
+    
+print(fruits)
+
+name = 'Umerh'
+
+for a in name:
+    print(name)
+    continue
+
+values = range(6,12)
+for i in values:
+    print(i)
+    
+a = 5
+while a<=5:
+    
+    print(a)
+   
+    
+ 
