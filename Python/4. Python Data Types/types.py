0c6bfc127adb1d5e3b3f7d56e8e34e6e61472578
uhassantoo <uhassantoor@gmail.com>
2025-01-02 11:48:18 +0500

    4. Data Types

---
+++ Python/4. Python Data Types/types.py
@@ -0,0 +1,43 @@
+hello = 123
+
+hello1 = {}
+
+hello2 = 12.2
+
+hello3 = ""
+hello4 = []
+hello5 = (  )
+
+hello6 = True   
+
+# Python Data types
+
+int1 = 26
+print('Numeric Data Type', int1)
+
+float1 = 12.2
+print('Numeric Data Types',float1)
+
+str1 = 'This is string data type'
+print('String Data Type', str1)
+
+list = ['umer',28,'DHA','Lahore']
+print('Sequence Data Type (List)', list)
+
+tuple1 = ('umer',28,'DHA')
+print('Sequence Data Type (tuple)',tuple1)
+
+range1 = range(2,12)
+print('Sequence Data Type (range)' ,range1)
+
+dict1 = {
+    'name': 'Umer',
+    'age': 28,
+}
+print('Sequence Data Type (Dict)' , dict1)
+
+bool1 = True
+print('Boolean Data type', bool1)
+
+set1 = {22,'hello'}
+print('Sets Data Type ', set1)
