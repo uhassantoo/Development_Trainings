eb991f0be2458dda1f06dff31a07bae055d022ed
uhassantoo <uhassantoor@gmail.com>
2025-01-07 12:56:13 +0500

    OOP Python

---
+++ Python/6. OOP Python Object & Class/classes.py
@@ -0,0 +1,33 @@
+# #Syntax
+# class className:
+#     #class objects
+
+class Car:
+    
+    #Attributes
+    name = ''
+    make = ''
+    model = ''
+    
+# create a class object
+# car2 = Car()
+
+# car2.make = 'Honda'
+# car2.name = 'City'
+
+# print(f'Make... {car2.make} , Name.... {car2.name}')
+
+suzuki = Car()
+
+suzuki.model = 2025
+suzuki.name = 'Swift'
+
+toyata = Car()
+
+toyata.make = 'Toyata Motors'
+
+bmw = Car()
+
+bmw.model = 'M Series'
+
+print(suzuki.model,suzuki.name)
